266cbca38a33f76b3f6a2ec1fedef4301d91f29e
Gindulin Ilia <gindulin.marselevich@gmail.com>
2024-12-15 14:00:56 +0300

    add script for counting clicks

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function() {
+    let count = 0;
+
+    document.getElementById('clickButton').addEventListener('click', function() {
+        count++;
+        document.getElementById('clickCount').innerText = count;
+    });
+});
